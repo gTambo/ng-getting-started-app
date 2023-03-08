4900360de288b3a3d40145cc5ee1e5838e0eaa9c
Greg Tambornino <greg.tambornino@concordusa.com>
2023-03-08 16:56:07 -0600

    commit from existing files developed on stackblitz

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService,
+    ) {}
+
+  ngOnInit() {
+    // first get the product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // find the product that corresponds with the id found in route
+    this.product = products.find(product => product.id === productIdFromRoute);
+
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('This product has been added to your a carta...');
+  }
+
+}
